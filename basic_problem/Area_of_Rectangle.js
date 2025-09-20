eed7c326809ba7565dc57156baabf32ae056a071
muhammad-bilal54 <muhammadbilal5412429@gmail.com>
2025-09-20 17:53:11 +0500

    initail commit

---
+++ basic_problem/Area_of_Rectangle.js
@@ -0,0 +1,7 @@
+function AreaOfRectangle (length , width) {
+    const Area = length * width ;
+    console.log("Area of rectangle is :" , Area);
+    
+}
+
+AreaOfRectangle(20,10);
--- basic_problem/Hello_world.js
+++ basic_problem/Hello_world.js
@@ -1,1 +1,12 @@
 console.log("Hello World!");
+
+// write a funtion that print sum of two number 
+
+function sum (a , b) {
+    const sum = a + b ;
+    console.log("sum of two number is :" , sum);
+    
+}
+
+sum(20,25) ;
+sum(23,34) ;
